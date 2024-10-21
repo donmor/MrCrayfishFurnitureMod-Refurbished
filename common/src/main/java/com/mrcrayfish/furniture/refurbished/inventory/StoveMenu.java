8b90bb90bb27a55bc04287f9afa1744a28e53264
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-10-21 23:30:48 +1030

    🐛 Fixed stove recipe boook not moving output

--- common/src/main/java/com/mrcrayfish/furniture/refurbished/inventory/StoveMenu.java
+++ common/src/main/java/com/mrcrayfish/furniture/refurbished/inventory/StoveMenu.java
@@ -173,7 +173,7 @@ public class StoveMenu extends SimpleRecipeContainerMenu<Container> implements I
     @Override
     public int getGridWidth()
     {
-        return 3;
+        return 1;
     }
 
     @Override
@@ -197,7 +197,7 @@ public class StoveMenu extends SimpleRecipeContainerMenu<Container> implements I
     @Override
     public boolean shouldMoveToInventory(int slot)
     {
-        return slot < this.getGridWidth() * this.getGridHeight();
+        return slot == 0 || slot == 3;
     }
 
     @Override
